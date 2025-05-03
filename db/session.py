b1bb0f4ce1a969f969fac536c881bea00c4f5ead
Cimer17 <ivanklopov2000@yandex.ru>
2025-05-03 22:14:35 +0300

    test db

--- db/session.py
+++ db/session.py
@@ -3,14 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-data_dir = "./data"
-
-# Проверка существования и создание при необходимости
-if not os.path.exists(data_dir):
-    os.makedirs(data_dir)
-
 # DATABASE_URL = "sqlite:///./db/guests.db"
-DATABASE_URL = "sqlite:///./data/guests.db"
+
+DATABASE_URL = "sqlite:////data/guests.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
