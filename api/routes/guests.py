84d516573c25a01014ad29388d8e0592abff3813
Cimer17 <ivanklopov2000@yandex.ru>
2025-05-03 21:31:29 +0300

    new crud

--- api/routes/guests.py
+++ api/routes/guests.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from schemas.guestsSC import GuestCreate
 from db.session import get_db
-from crud.guest_crud import create_guest, get_all_guests
+from crud.guest_crud import check_phone_guest, create_guest, dellete_guest, get_all_guests
 
 router = APIRouter()
 
@@ -13,3 +13,11 @@ def create_user(guest: GuestCreate, db: Session = Depends(get_db)):
 @router.get("")
 def read_guests(db: Session = Depends(get_db)):
     return get_all_guests(db)
+
+@router.delete("/{guest_id}")
+def delete_user(guest_id:int, db: Session = Depends(get_db)):
+    return dellete_guest(db, guest_id)
+
+@router.get("/check/{phone}")
+def check_phone(phone: str, db: Session = Depends(get_db)):
+    return check_phone_guest(db, phone)
--- crud/guest_crud.py
+++ crud/guest_crud.py
@@ -17,6 +17,22 @@ def create_guest(db: Session, guest: GuestCreate):
     db.refresh(db_guest)
     return {"message": "User added successfully", "user": guest}
 
+def dellete_guest(db: Session, guest_id: int):
+    db_guest = db.query(Guests).filter(Guests.id == guest_id).first()
+    if db_guest:
+        db.delete(db_guest)
+        db.commit()
+        return {"message": "Гость удален"}
+    else:
+        return {"message": "Гость не найден"}
+
+def check_phone_guest(db: Session, phone: str):
+    db_guest = db.query(Guests).filter(Guests.phone == phone).first()
+    if db_guest:
+        return {"message": "Yes", "guest": db_guest}
+    else:
+        return {"message": "No"}
+
 def get_all_guests(db: Session):
     guests = db.query(Guests).all()
     return {"guests": guests}
